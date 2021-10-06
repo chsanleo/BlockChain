28137bf6a6de27bac2c9b9f93d44a25288f7b0e7
Christian <chsanleo@gmail.com>
2021-10-06 18:14:46 +0200

    import operation router to index

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+const operationRouter = require('./routers/operationsRouter.js');
+app.use('/operation', operationRouter);
 
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
